dea9a1ddfbde43413545093a33b1f415125e546c
Diego <diego.campana.1801@gmail.com>
2025-05-09 16:24:46 -0500

    prueba git

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default function DashboardPage() {
         <SignedOut>
           <p className="mt-4">Por favor, inicia sesión para acceder al dashboard.</p>
           <Link href="/sign-in">
-            <Button className="mt-4">Iniciar sesión</Button>
+            <Button className="mt-4">Iniciar sesiónn</Button>
           </Link>
         </SignedOut>
       </div>
