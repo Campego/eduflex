dfc679dfc76475a061a8d4d85ec5907b557c157d
Diego <diego.campana.1801@gmail.com>
2025-05-09 16:22:18 -0500

    Login

---
+++ app/(login)/header.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState } from "react";
+
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useAuth,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { links } from "@/config";
+import { cn } from "@/lib/utils";
+
+
+export const Header = () => {
+  const { isSignedIn } = useAuth();
+
+  return (
+    <>
+
+
+      <header>
+        <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
+          <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
+            <Image src="/mascot.svg" alt="Logo" height={40} width={40} />
+
+            <h1 className="text-2xl font-extrabold tracking-wide text-orange-400">
+              Eduflex
+            </h1>
+          </Link>
+
+          <div className="flex gap-x-3">
+            <ClerkLoading>
+              <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+            </ClerkLoading>
+            <ClerkLoaded>
+              <SignedIn>
+                <UserButton afterSignOutUrl="/" />
+              </SignedIn>
+
+              <SignedOut>
+                <SignInButton
+                  mode="modal"
+                  afterSignInUrl="/dashboard"
+                  afterSignUpUrl="/dashboard"
+                >
+                  <Button size="lg" variant="ghost">
+                    Ingresar
+                  </Button>
+                </SignInButton>
+              </SignedOut>
+
+            </ClerkLoaded>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
---
+++ config/index.ts
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Eduflex",
+} as const;
+
+export const links = {
+
+} as const;
